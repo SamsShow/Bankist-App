dddfba673f16ebb053a19f90b21ed3dd35b95f81
Saksham Tyagi <sakshamtyagi2008@gmail.com>
2023-08-27 19:01:43 +0530

    Usernames

--- script.js
+++ script.js
@@ -80,6 +80,14 @@ const displayMovements = function(movements){
 
 displayMovements(account1.movements);
 
+const createUsernames = function (accs){
+  accs.forEach(function(acc){
+    acc.username = acc.owner.tolowercase().split(' ').map(name => name[0]).join('');
+  });
+};
+
+createUsernames(accounts);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
