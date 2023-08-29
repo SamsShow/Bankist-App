01c0b0593ebaf95af74703e79880ca01c5e0cfd6
Saksham Tyagi <sakshamtyagi2008@gmail.com>
2023-08-29 22:09:54 +0530

    Field Update

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ const displayMovements = function(movements){
     const html = `<div class="movements">
       <div class="movements__row">
       <div class="movements__type movements__type--${type}">${i+1} ${type}</div>
-      <div class="movements__value">${mov}</div>`;
+      <div class="movements__value">${mov}€</div>`;
 
       containerMovements.insertAdjacentHTML('afterbegin', html);
   });
@@ -86,6 +86,26 @@ const calcDisplayBalance = function(movements){
 }
 
 calcDisplayBalance(account1.movements);
+
+const calcDisplaySummary = function(movements){
+  const income = movements.filter(mov => mov>0).reduce((acc,mov) => acc+mov,0);
+  labelSumIn.textContent = `${income}€`;
+
+  const out = movements.filter(mov => mov < 0 ).reduce((acc,mov) => acc + mov,0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = acc.movements
+  .filter(mov => mov > 0)
+  .map(deposit => (deposit * acc.interestRate) / 100)
+  .filter((int, i, arr) => {
+    // console.log(arr);
+    return int >= 1;
+  })
+  .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}€`;
+}
+
+calcDisplaySummary(account1.movements);
 
 const createUsernames = function (accs){
   accs.forEach(function(acc){
